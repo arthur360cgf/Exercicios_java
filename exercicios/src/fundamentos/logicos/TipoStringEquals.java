25a54f177ddafec190863b7e2c2c7c30d1f5f917
Arthur Aragão Mattei Moraes <68078154+arthur360cgf@users.noreply.github.com>
2022-09-14 16:55:29 -0300

    Add files via upload

---
+++ exercicios/src/fundamentos/logicos/TipoStringEquals.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class TipoStringEquals {
+	public static void main(String[] args) {
+		
+		System.out.println("2" == "2");
+		
+		String s = new String("2");
+		System.out.println("2" == s);
+		System.out.println("2".equals(s));
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String s2 = entrada.next();
+		System.out.println("2" == s2.trim());
+		//.trim() é uma função que ignora os espaços em brancos colocados no Scanner 
+		System.out.println("2".equals(s2.trim()));
+		
+		entrada.close();
+	}
+
+}
