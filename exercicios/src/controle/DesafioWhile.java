8409cafca2140e957ba54acffcd0e7c3934d81e0
Arthur Aragão Mattei Moraes <68078154+arthur360cgf@users.noreply.github.com>
2022-09-21 18:24:53 -0300

    Add files via upload

--- exercicios/src/controle/DesafioWhile.java
+++ exercicios/src/controle/DesafioWhile.java
@@ -41,5 +41,57 @@ public class DesafioWhile {
 		
 	sc.close();
 		
-	}
+}
+	
+	/**public void resposta() {
+		Scanner sc = new Scanner(System.in);
+		int quantidadeDeNotas = 0;
+		double nota = 0;
+		double total = 0;
+		
+		while(nota != -1) {
+			System.out.println("Informe a nota (ou -1 p/ sair)");
+			nota = sc.nextDouble();
+			
+			if(nota <= 10 && nota >= 0) {
+				total += nota;
+				quantidadeDeNotas++;
+				
+			}else if(nota != -1) {
+				System.out.println("Nota invalida!!!");
+			}
+		}
+		double media = total / quantidadeDeNotas;
+		System.out.println("Media = " + media);
+		
+		sc.close();
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
